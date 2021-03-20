64b5490bafc30e3d99dee515241730b30b25fe0a
Santiago Laiton <santiago.laiton@mail.escuelaing.edu.co>
2021-03-19 23:33:22 -0500

    PARTE DOS

--- src/main/java/edu/eci/arsw/collabpaint/controller/PointsController.java
+++ src/main/java/edu/eci/arsw/collabpaint/controller/PointsController.java
@@ -1,16 +1,16 @@
-package edu.eci.arsw.collabpaint.controller;
-
-import edu.eci.arsw.collabpaint.model.Point;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class PointsController {
-    @MessageMapping("/newpoint")
-    @SendTo("/topic/newpoint")
-    public Point pointStomp (Point point) throws Exception{
-
-        return new Point(point.getX(),point.getY());
-    }
-}
+//package edu.eci.arsw.collabpaint.controller;
+//
+//import edu.eci.arsw.collabpaint.model.Point;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.SendTo;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class PointsController {
+//    @MessageMapping("/newpoint")
+//    @SendTo("/topic/newpoint")
+//    public Point pointStomp (Point point) throws Exception{
+//        System.out.println("IT'S HERE!!!"+point.getX());
+//        return new Point(point.getX(),point.getY());
+//    }
+//}
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,4 +1,5 @@
 var app = (function () {
+    
 
     class Point{
         constructor(x,y){
@@ -8,6 +9,21 @@ var app = (function () {
     }
     
     var stompClient = null;
+
+    var initMouseEvent = function(){
+        let mouseInteractionCanvas = document.getElementById("canvas").getContext('2d');
+        
+
+        mouseInteractionCanvas.canvas.addEventListener('click', function(event){
+
+            var mouseX = event.clientX - mouseInteractionCanvas.canvas.offsetLeft;
+            var mouseY = event.clientY - mouseInteractionCanvas.canvas.offsetTop;
+
+            stompClient.send("/topic/newpoint",{},JSON.stringify(new Point(mouseX, mouseY)));
+            
+        });
+
+    }
 
     var addPointToCanvas = function (point) {        
         var canvas = document.getElementById("canvas");
@@ -21,6 +37,8 @@ var app = (function () {
     var getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
+        console.log(evt.clientX - rect.left);
+        console.log(evt.clientY - rect.top);
         return {
             x: evt.clientX - rect.left,
             y: evt.clientY - rect.top
@@ -39,8 +57,10 @@ var app = (function () {
             console.log('Connected: ' + frame);
             
             stompClient.subscribe('/topic/newpoint', function (message) {
-                var objectJS=JSON.parse(message.body);
-                alert("Punto X: "+objectJS.x+" Punto T: "+objectJS.y);
+                var messagePoint=JSON.parse(message.body);
+                // alert("Punto X: "+objectJS.x+" Punto T: "+objectJS.y); //first commit
+                addPointToCanvas(messagePoint);
+
             });
         });
 
@@ -54,13 +74,15 @@ var app = (function () {
             var can = document.getElementById("canvas");
             
             //websocket connection
+
             connectAndSubscribe();
+            initMouseEvent();
         },
 
         publishPoint: function(px,py){
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
-            addPointToCanvas(pt);
+            // addPointToCanvas(pt);
 
             //publicar el evento
             
